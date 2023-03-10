6a8dfe2e7338db89e9ec299252dc2f48856b0e4f
xbfinal <358863605@qq.com>
2023-03-10 19:33:13 +0800

    单列模式-枚举

---
+++ src/Creative_mode/Singleton_mode/Enumeration.java
@@ -0,0 +1,31 @@
+package Creative_mode.Singleton_mode;
+
+/**
+ * @Title: Enumeration
+ * @Author 笑霸fianl
+ * @Package Creative_mode.Singleton_mode
+ * @Date 2023/3/10 19:24
+ * @描述: 枚举
+ */
+public class Enumeration {
+    public static void main(String[] args) {
+
+        Singleton singleton1 = Singleton.INSTANCE;
+        Singleton singleton2 = Singleton.INSTANCE;
+        System.out.println(singleton1==singleton2);
+        System.out.println(singleton1.hashCode());
+        System.out.println(singleton2.hashCode());
+    }
+}
+
+
+/**
+ * 枚举========================
+ *
+ * 借助JDK1.5中添加的枚举来实现单例模式。不仅能避免多线程同步问题，而且还能防止反序列化重新创建新的对象
+ */
+enum Singleton{
+    //属性
+    INSTANCE;
+    public void sayOK(){}
+}
