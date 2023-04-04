ef30b8c812727f094c058aa45201121518f14f9a
xbfinal <358863605@qq.com>
2023-04-04 10:05:12 +0800

    ReentrantLock

--- 并发学习/src/com/xbfina/myJUC/lock_/MyLock.java
+++ 并发学习/src/com/xbfina/myJUC/lock_/MyLock.java
@@ -1,5 +1,7 @@
 package com.xbfina.myJUC.lock_;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @Title: MyLock
  * @Author 笑霸fianl
@@ -8,4 +10,63 @@ package com.xbfina.myJUC.lock_;
  * @描述:
  */
 public class MyLock {
+    public static void main(String[] args) {
+
+        LTicket lTicket = new LTicket();
+
+        new Thread(()->{
+            Thread.currentThread().setName("AA");
+            for(int i = 0 ;i<60;i++){
+                lTicket.sale();
+            }
+        }).start();
+
+        new Thread(()->{
+            Thread.currentThread().setName("BB");
+            for(int i = 0 ;i<60;i++){
+                lTicket.sale();
+            }
+        }).start();
+
+    }
+
+}
+//创建资源类
+class LTicket{
+    //资源
+    private int nums = 50;
+
+    //创建可重入锁
+    private final ReentrantLock lock  = new ReentrantLock();
+
+
+    //消费资源的方法
+    public void sale() {
+        if(this.nums <= 0){
+            return;
+        }
+
+        try{
+            Thread.sleep(50);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        try{
+            lock.lock();
+            if(this.nums > 0){
+
+                System.out.println("当前资源数量为："+this.nums+"当前线程名："+Thread.currentThread().getName());
+                this.nums--;
+
+            }
+        }finally {
+
+            lock.unlock();
+        }
+
+
+    }
+
 }
