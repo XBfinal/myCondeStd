b4040f001975826124888e846d744b4d28ebee14
xbfinal <358863605@qq.com>
2023-05-02 21:52:10 +0800

    第18题. 四数之和    https://leetcode.cn/problems/4sum/

---
+++ 算法学习/src/哈希表/demo07.java
@@ -0,0 +1,79 @@
+package 哈希表;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Title: demo07
+ * @Author 笑霸fianl
+ * @Package 哈希表
+ * @Date 2023/5/2 21:46
+ * @描述: 第18题. 四数之和    https://leetcode.cn/problems/4sum/
+ */
+public class demo07 {
+
+
+    /**
+     * 给你一个由 n 个整数组成的数组nums ，和一个目标值 target 。
+     * 请你找出并返回满足下述全部条件且不重复的四元组[nums[a], nums[b], nums[c], nums[d]]
+     * （若两个四元组元素一一对应，则认为两个四元组重复）：
+     *
+     * 0 <= a, b, c, d< n
+     * a、b、c 和 d 互不相同
+     * nums[a] + nums[b] + nums[c] + nums[d] == target
+     * 你可以按 任意顺序 返回答案 。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/4sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // nums[i] > target 直接返回, 剪枝操作
+            if (nums[i] > 0 && nums[i] > target) {
+                return result;
+            }
+
+            if (i > 0 && nums[i - 1] == nums[i]) {    // 对nums[i]去重
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (j > i + 1 && nums[j - 1] == nums[j]) {  // 对nums[j]去重
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (right > left) {
+                    // nums[k] + nums[i] + nums[left] + nums[right] > target int会溢出
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum > target) {
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        // 对nums[left]和nums[right]去重
+                        while (right > left && nums[right] == nums[right - 1]) right--;
+                        while (right > left && nums[left] == nums[left + 1]) left++;
+
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
