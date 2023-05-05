c768570c6a9c70e5cb0326570902475ca80a5628
xbfinal <358863605@qq.com>
2023-05-05 20:17:05 +0800

    27. 移除元素 https://leetcode.cn/problems/remove-element/ //相向双指针

--- 算法学习/src/双指针/Demo01.java
+++ 算法学习/src/双指针/Demo01.java
@@ -11,7 +11,9 @@ public class Demo01 {
 
     public static void main(String[] args) {
 
+        int[] ints = {0,1,2,2,3,0,4,2};
 
+        System.out.println(removeElement2(ints,2));
     }
 
     //快慢双指针
@@ -27,6 +29,27 @@ public class Demo01 {
 
         }
         return s;
+
+    }
+
+    //相向双指针
+    public static int removeElement2(int[] nums, int val) {
+        int len =nums.length;
+        int left=0,right=len-1;
+        while(right >= 0 && nums[right] == val) right--;
+        while(left <= right) {
+
+            if(nums[left] == val) { //left位置的元素需要移除
+                //将right位置的元素移到left（覆盖），right位置移除
+                nums[left] = nums[right];
+                right--;
+            }
+            left++;
+            while(right >= 0 && nums[right] == val) right--;
+
+        }
+        return left;
+
 
     }
 }
