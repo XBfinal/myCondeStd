28a70d783495cecc26c9e5472940c7be9b2eb906
xbfinal <358863605@qq.com>
2023-05-15 15:53:00 +0800

    粘包 半包 解析

---
+++ nettyStd/netty-demo/src/test/java/com/xbfinal/netty/c1/TestByBufferDemo01.java
@@ -0,0 +1,54 @@
+package com.xbfinal.netty.c1;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Title: TestByBufferDemo01
+ * @Author 笑霸fianl
+ * @Package com.xbfinal.netty.c1
+ * @Date 2023/5/15 15:28
+ * @描述: 粘包 半包 解析
+ */
+public class TestByBufferDemo01 {
+
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(50);
+        buffer.put("hello\nworld\nmy name is ".getBytes()); //粘包 半包
+
+        hello(buffer);
+        buffer.put("my name is xbfinal!\n".getBytes());// 半包
+        hello(buffer);
+    }
+
+    public static void hello(ByteBuffer buffer){
+        //进来先转化为读模式
+        buffer.flip();
+        for (int i = buffer.position(); i <buffer.limit() ; i++) {
+
+
+            if('\n' == buffer.get(i)){
+                // 我们先存入新的ByteBuffer
+                int len = i -buffer.position();
+                ByteBuffer allocate = ByteBuffer.allocate(len);
+                for (int j = 0; j < len; j++) {
+                    allocate.put(buffer.get());
+                }
+                //输出
+                allocate.flip();
+                for (int j = 0; j < len; j++) {
+                    System.out.print((char) allocate.get());
+                }
+            }
+
+
+
+        }
+        //这里由于有粘包 半包。不能用clear
+        buffer.compact();
+
+
+    }
+
+}
