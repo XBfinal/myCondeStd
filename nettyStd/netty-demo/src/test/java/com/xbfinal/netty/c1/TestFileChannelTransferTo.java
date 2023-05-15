d87818282798f79e56abd23ec1f75a190f0f3d0c
xbfinal <358863605@qq.com>
2023-05-15 16:10:04 +0800

    两个 FileChannel 传数据

---
+++ nettyStd/netty-demo/src/test/java/com/xbfinal/netty/c1/TestFileChannelTransferTo.java
@@ -0,0 +1,31 @@
+package com.xbfinal.netty.c1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ * @Title: TestFileChannerlTransferTo
+ * @Author 笑霸fianl
+ * @Package com.xbfinal.netty.c1
+ * @Date 2023/5/15 16:03
+ * @描述: 两个 FileChannel 传数据
+ */
+public class TestFileChannelTransferTo {
+
+    public static void main(String[] args) {
+
+
+        try(FileChannel from = new FileInputStream("nettyStd/netty-demo/src/main/resources/data.txt").getChannel();
+            FileChannel to = new FileOutputStream("nettyStd/netty-demo/src/main/resources/to1.txt").getChannel()
+        ) {
+            from.transferTo(0,from.size(),to);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
