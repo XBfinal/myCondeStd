d951a533296ddc5a6ebc5d5749675c796cd5e6bd
xbfinal <358863605@qq.com>
2023-05-20 09:52:26 +0800

    搭建springboot 环境

---
+++ StdSpring/src/main/java/com/xbfinal/stdspring/StdSpringApplication.java
@@ -0,0 +1,13 @@
+package com.xbfinal.stdspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StdSpringApplication.class, args);
+    }
+
+}
