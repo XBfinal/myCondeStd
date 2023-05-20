c601e3100e2d262532e387c4ae5e0385cea23361
xbfinal <358863605@qq.com>
2023-05-20 10:11:55 +0800

    环境搭建  lombok

--- StdSpring/src/main/java/com/xbfinal/stdspring/StdSpringApplication.java
+++ StdSpring/src/main/java/com/xbfinal/stdspring/StdSpringApplication.java
@@ -1,13 +1,20 @@
 package com.xbfinal.stdspring;
 
+import com.xbfinal.stdspring.component.component1;
+import com.xbfinal.stdspring.component.component2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StdSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StdSpringApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(StdSpringApplication.class, args);
+
+
+        run.getBean(component2.class).sentListener();
+
     }
 
 }
